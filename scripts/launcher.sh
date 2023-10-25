1ab9887a64c86e569a978babf45c2afbe4ac7391
itsKorzie <romain.croughs@gmail.com>
2023-10-25 15:12:56 +0200

    feature: launcher script

---
+++ scripts/launcher.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [[ $1 = "-i" || $1 = "--interactive" ]]
+then
+  # Mode interactif
+  ./img-search $2
+elif [[ $1 = "-a" || $1 = "--automatic" ]]
+then
+  # Mode automatique
+  ./list-file.sh $3 > ./img-search $2
+fi
