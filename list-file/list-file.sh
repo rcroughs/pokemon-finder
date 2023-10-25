468cf61ee902d5280b15bcbd610b8c9480dbf786
itsKorzie <romain.croughs@gmail.com>
2023-10-25 14:33:19 +0200

    docs: explication du list-file

--- list-file/list-file.sh
+++ list-file/list-file.sh
@@ -2,13 +2,16 @@
 
 if [[ -z $1 ]]
 then
+  # Dans le cas où il n'y a pas de paramètre
   echo "Missing directory name."
   exit 1
 elif [[ ! -d $1 ]]
 then
+  # Dans le cas où le directory n'existe pas
   echo "The specified path is not a directory"
   exit 2
 else
+  # Execution classique
   find $1 -type f -maxdepth 1
   exit 0
 fi
