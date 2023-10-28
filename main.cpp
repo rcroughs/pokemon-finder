35d39eeca5b27f982e0a51d25e3095bcda657408
itsKorzie <romain.croughs@gmail.com>
2023-10-28 15:45:41 +0200

    fix: code design readjusted

--- main.cpp
+++ main.cpp
@@ -3,13 +3,27 @@
 #include <string>
 #include <iostream>
 
-int main(int argc, char* argv[]) {
-  const char image_to_find = *argv[1];
-  std::vector<std::string> images_set;
+void exection_enfant(const char &image_to_find) {
+  // Execution des enfants (ils fonctionnent de la meme manière)
+}
+
+void execution_parent() {
+  // Execution du processus parent
   char buffer[999];
   while (fgets(buffer, 999, stdin)) {
     std::string new_path(buffer);
-    images_set.push_back(new_path); 
+    // Communication aux enfants des adresses
+
   };
+  // Envoie des signaux pour les enfants
+
+}
+
+int main(int argc, char* argv[]) {
+  const char image_to_find = *argv[1];
+  // Creation des deux processus, de la mémoire partagée 
+  // et des pipes de communication
+
+
   return 0;
 }
